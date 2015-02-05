1f953ba7352f425d9a1892ec571f12afc0cea539
Sergio Meligrana <sergio.meligrana@gmail.com>
2015-02-05 20:50:53 +0100

    test

--- step1/HelloWorld.java
+++ step1/HelloWorld.java
@@ -1,31 +1,8 @@
 public class HelloWorld {
 	public static void main(String[] args){
 		HelloWorld hw = new HelloWorld();
-		hw.echo();
-
-		int sum = hw.sum(2, 3);
-		System.out.println(sum);
-
-		HelloWorld hw2 = new HelloWorld();
-
-		System.out.println("hw "+hw.increment());
-		System.out.println("hw2 "+hw2.increment());
-		System.out.println("hw2 "+hw2.increment());
-		System.out.println("hw2 "+hw2.increment());
-		System.out.println("hw "+hw.increment());
-		System.out.println("hw "+hw.increment());
-	}
-
-	void echo(){
 		System.out.println("Hello World");
 	}
 
-	int sum(int a, int b){
-		return a +b;
-	}
-
-	private int contatore = 0;
-	int increment(){
-		return contatore++;
-	}
+	
 }
